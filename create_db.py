fc4568ba1b6e3118973a66158ac464ecfd4d3ccc
sofiajanereobaldes <24-36961@g.batstate-u.edu.ph>
2025-12-18 17:33:03 +0800

    Remove all files for fresh start

--- create_db.py
+++
@@ -1,9 +0,0 @@
-from app import app, db
-from extensions import db
-
-with app.app_context():
-    db.create_all()
-    print("Tables created/updated.")
-    # db.session.execute("ALTER TABLE announcements ADD COLUMN date_posted DATETIME;")
-    # db.session.execute("UPDATE announcements SET date_posted = CURRENT_TIMESTAMP WHERE date_posted IS NULL;")
-    # db.session.commit()
